9da117b4e9bace17b4f53881623868d2e31e5da8
Finn-Eschler <scitech@eclipso.de>
2026-03-23 16:23:32 +0100

    Durchschnittswerte

--- durchschnittswerte.py
+++ durchschnittswerte.py
@@ -13,10 +13,12 @@ stations = ms.stations.nearby(POINT, limit=3)
 ts = ms.daily(stations, START, END)
 df = ms.interpolate(ts, POINT).fetch()
 
-# Kalendertag extrahieren
+# Durchschnittliches Profil mit 365 Tagen pro Jahr
+df = df[~((df.index.month == 2) & (df.index.day == 29))]
 df["dayofyear"] = df.index.dayofyear
-
-# Durchschnittliches Profil
 profile = df.groupby("dayofyear")["tsun"].mean()
+
+
+profile.to_frame().to_parquet("dresden_tsun_profile.parquet")
 
 print(profile)
